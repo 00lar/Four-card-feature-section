ceb5876ed550c30c3f568460b4be4b2e72f58080
00lar <eduardani84@gmail.com>
2023-02-03 23:40:32 -0700

    Fonts and vars

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{html,js}", "./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      borderColor: {
+        orange: "hsl(0, 78%, 62%)",
+        cyan: "hsl(180, 62%, 55%)",
+        orange: "hsl(34, 97%, 64%)",
+        Blue: "hsl(212, 86%, 64%)",
+      },
+      textColor: {
+        "Dark-blue": "hsl(234, 12%, 34%)",
+        "Grayish-blue": "hsl(229, 6%, 66%)",
+        "Gray-light": "hsl(0, 0%, 98%)",
+      },
+      fontFamily: {
+        Poppins: ["Poppins", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
+};
